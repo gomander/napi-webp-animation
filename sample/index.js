58c28e3c545a2ab479aee80495c29eba2e0301fd
Gunnar Omander <gunnar@gomander.dev>
2025-01-02 21:05:57 +0100

    Compare WebP encoder with GIF encoder in sample project

--- sample/index.js
+++ sample/index.js
@@ -2,24 +2,63 @@
 
 import { WebpEncoder } from '../index.js'
 import { Canvas } from '@napi-rs/canvas'
+import { GIFEncoder } from '@gomander/napi-gif-encoder'
 
-const encoder = new WebpEncoder(100, 100, 'output.webp', { loopCount: 1, lossless: true, quality: 100, method: 6 })
+function encodeWebp() {
+  const canvas = new Canvas(100, 100)
+  const ctx = canvas.getContext('2d')
 
-const canvas = new Canvas(100, 100)
-const ctx = canvas.getContext('2d')
-ctx.fillStyle = 'red'
-ctx.fillRect(0, 0, 100, 100)
+  const encoder = new WebpEncoder(100, 100)
 
-encoder.addFrame(canvas.data(), 1000)
+  encoder.setFrameRate(3)
 
-ctx.fillStyle = 'green'
-ctx.fillRect(0, 0, 100, 100)
+  console.time('Encoding WebP')
+  ctx.fillStyle = 'red'
+  ctx.fillRect(0, 0, 100, 100)
 
-encoder.addFrame(canvas.data(), 1000)
+  encoder.addFrame(canvas.data())
 
-ctx.fillStyle = 'blue'
-ctx.fillRect(0, 0, 100, 100)
+  ctx.fillStyle = 'green'
+  ctx.fillRect(0, 0, 100, 100)
 
-encoder.addFrame(canvas.data(), 1000)
+  encoder.addFrame(canvas.data())
 
-encoder.finish(2000)
+  ctx.fillStyle = 'blue'
+  ctx.fillRect(0, 0, 100, 100)
+
+  encoder.addFrame(canvas.data())
+
+  encoder.writeToFile('output.webp')
+  console.timeEnd('Encoding WebP')
+}
+
+async function encodeGif() {
+  const canvas = new Canvas(100, 100)
+  const ctx = canvas.getContext('2d')
+
+  const gifEncoder = new GIFEncoder(100, 100, 'output.gif')
+
+  gifEncoder.setFrameRate(3)
+
+  console.time('Encoding GIF')
+  ctx.fillStyle = 'red'
+  ctx.fillRect(0, 0, 100, 100)
+
+  gifEncoder.addFrame(structuredClone(canvas.data()))
+
+  ctx.fillStyle = 'green'
+  ctx.fillRect(0, 0, 100, 100)
+
+  gifEncoder.addFrame(structuredClone(canvas.data()))
+
+  ctx.fillStyle = 'blue'
+  ctx.fillRect(0, 0, 100, 100)
+
+  gifEncoder.addFrame(structuredClone(canvas.data()))
+
+  await gifEncoder.finish()
+  console.timeEnd('Encoding GIF')
+}
+
+encodeWebp()
+encodeGif()
